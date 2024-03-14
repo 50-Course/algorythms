1b09f17eae6dc85e23caa8092365f283a879fbcb
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-03-14 19:45:50 +0100

    some old Node datastructure

---
+++ javascript/Node.ts
@@ -0,0 +1,11 @@
+// the algorithm
+class Node {
+    val: number;
+    left: Node;
+    right: Node;
+    constructor(val: number) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
