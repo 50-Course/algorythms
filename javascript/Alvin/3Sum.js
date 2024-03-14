6cdd42d95c458dfe9cc5c70f94c1eef7901e929b
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-03-14 19:08:04 +0100

    3Sum problem with BFS, could be done with DFS too

---
+++ javascript/Alvin/3Sum.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+/**
+ * Computes the tatal sum of nodes in a tree that is equal to some
+ * target sum.
+ * 
+ * Uses a BFS approach (recursive approach)
+ * 
+ * {@param} node Tree-like structure with nodes
+ * {@param} target total sum which some node must be equivalent to
+ * {@returns} result sum of nodes of a given tree
+ */
+const ThreeSum = (node) => {
+    if (node === null) return 0;
+    return node.val + ThreeSum(node.left) + ThreeSum(node.right);
+}
+
+// Iterative approach
+const threeSum= (node) => {
+    if (node === null) return 0;
+    let queue = 0;
+    let sum = 0;
+    while(queue.length > 0) {
+        let current = queue.shift()
+        sum += current.val;
+
+        if (current.left !== null) queue.push(current.left);
+        if (current.right !== null) queue.push(current.right);
+    }
+    return sum;
+}
+
+let a = new Node(1);
+let b = new Node(2);
+let c = new Node(3);
+let d = new Node(4);
+let e = new Node(5);
+let f = new Node(6);
+let g = new Node(7);
+
+
+a.left = b;
+a.right = c;
+a.left.right = d;
+a.left.left = g;
+b.right = f;
+b.left = e;
+
+
+console.log(threeSum(a));
+
+module.exports = {
+    ThreeSum
+}
