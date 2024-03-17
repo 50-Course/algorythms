56a208066b57008041355af113abcdab65dc0416
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-03-17 10:41:12 +0100

    Finding minimum value of a node in a given tree - recursive approach - DFS

--- javascript/Alvin/TreeMin.js
+++ javascript/Alvin/TreeMin.js
@@ -10,6 +10,7 @@ class Node {
 
 /**
  * Finds the smallest minimum value in a given tree
+ * using iterative BFS approach
  * @param {Node} node 
  * @returns {Node} node value in agiven tree structure 
  */
@@ -23,13 +24,38 @@ const TreeMin = node => {
     while(queue.length > 0) {
         let current = queue.shift();
 
-        if (current.val !== null && current.val <= minimums) minimums = current.val;
+        if (current.val !== null && current.val < minimums) minimums = current.val;
 
         if (current.right !== null) queue.push(current.right)
         if (current.left !== null) queue.push(current.left)
     }
     return minimums;
 
+}
+
+// Tree Sum problem using iterative Depth-First Search
+const treeMin = (node) => {
+    let minimums = Number.POSITIVE_INFINITY;
+
+    let stack = [ node ];
+
+    while( stack.length > 0) {
+        let current = stack.pop();
+
+        if (current.val < minimums && current.val != null) minimums = current.val;
+        if (current.left !== null) stack.push(current.left);
+        if (current.right !== null) stack.push(current.right);
+        
+    }
+    return minimums;
+}
+
+const treeMinRecursive = (node) => {
+    let minimums = 0;
+
+    values = current.val + treeMinRecursive(current.left) + treeMinRecursive(current.right);
+
+    return values < minimums ? current.val : Number.POSITIVE_INFINITY;
 }
 
 let a = new Node(1);
