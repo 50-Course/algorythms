6f744e8b5ff53298fec89a56049c5adaac419417
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-04-01 02:43:16 +0100

    feat: grid traveller problem with dynamic programming and tabulation

--- javascript/Alvin/tabulation-dp/GridTraveller.js
+++ javascript/Alvin/tabulation-dp/GridTraveller.js
@@ -10,14 +10,28 @@ const gridTraveller = (m, n) => {
   const table = new Array(m + 1).fill().map(() => new Array(n + 1).fill(0));
   table[1][1] = 1;
 
-  console.log(table);
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      const current = table[i][j];
+
+      // check if our current position is within bounds, so we don't perform
+      // illegal operation, and we add the current position to the next value
+      // we can only do this "rightward" and "downward'"
+      if (j + 1 <= n) table[i][j + 1] += current; // add downwards
+      if (i + 1 <= m) table[i + 1][j] += current; // add right
+    }
+  }
+  // console.table(table);
+  return table[m][n];
 };
 
 console.log(gridTraveller(1, 1));
 console.log(gridTraveller(2, 3));
 console.log(gridTraveller(3, 2));
+console.log(gridTraveller(3, 3));
 console.log(gridTraveller(10, 10));
 console.log(gridTraveller(4, 5));
+console.log(gridTraveller(18, 18));
 
 module.exports = {
   gridTraveller,
