934620b2532698e0f31f6ab76b6d9c1195d002d7
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-04-26 15:01:52 +0100

    problem: remove duplicates from arrays

---
+++ removeDups.js
@@ -0,0 +1,7 @@
+// We only returning the length of the new array
+//
+// We can only identify dups by comparing if we seen the number before
+const removeDups = (array) => {
+}
+
+console.log(removeDups([2, 2, 2, 11]))
